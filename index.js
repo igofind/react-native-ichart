26e599c015c41dad3c9e96c5bdc082347ad7e490
Tri Nguyen <nvutri@gmail.com>
2016-02-03 14:15:51 -0600

    Allow receiving dynamic props on wrapper

--- index.js
+++ index.js
@@ -4,31 +4,56 @@ import React from 'react-native';
 import ichart from './lib/ichart-wrapper.js';
 
 const {
-    View,
-    WebView,
-} = React;
+  View,
+  WebView,
+  } = React;
 
 export default React.createClass({
-    propTypes: {
-        render: React.PropTypes.func.isRequired
-    },
-    render: function() {
-        let renderString = this.props.render.toString();
-        return (
-            <View>
-                <WebView
-                    html={this.createHTML(renderString)}
-                    style={this.props.style}
-                    automaticallyAdjustContentInsets={false}
-                    contentInset={{top: 0, right: 0, bottom: 0, left: 0}}
-                />
-            </View>
-        );
-    },
-    createHTML: function(render) {
-        var html = `
+  propTypes: {
+    chart_type: React.PropTypes.string.isRequired,
+    data: React.PropTypes.array.isRequired,
+    title: React.PropTypes.string.isRequired,
+    width: React.PropTypes.number.isRequired,
+    height: React.PropTypes.number.isRequired,
+    coordinate: React.PropTypes.object.isRequired,
+    labels: React.PropTypes.array.isRequired
+  },
+  /**
+   * Create a Javascript static string to be injected to HTML.
+   * @returns {*}
+   */
+  renderChart: function () {
+    return `
+      var chart = new iChart.${this.props.chart_type}({
+        render: 'canvas-container',
+        data: ${JSON.stringify(this.props.data)},
+        title: '${this.props.title}',
+        width: ${this.props.width},
+        height: ${this.props.height},
+        coordinate: ${JSON.stringify(this.props.coordinate)},
+        labels: ${JSON.stringify(this.props.labels)}
+      });
+      chart.draw();
+      `;
+  },
+  render: function () {
+    let renderString = this.renderChart();
+    return (
+      <View>
+        <WebView
+          html={this.createHTML(renderString)}
+          style={this.props.style}
+          automaticallyAdjustContentInsets={false}
+          contentInset={{top: 0, right: 0, bottom: 0, left: 0}}
+          />
+      </View>
+    );
+  },
+  createHTML: function (renderJS) {
+    var html = `
             <DOCTYPE html>
             <html>
+
                 <head>
                     <title>iChart</title>
                     <meta charset="utf-8"/>
@@ -48,13 +73,15 @@ export default React.createClass({
                     <div id="canvas-container"></div>
                     <script>
                         $(function(){
-                            (${render})();
+                          ${renderJS}
                         });
                     </script>
                 </body>
             </html>
         `;
-        return html;
-    }
+    return html;
+  }
 });
+
+
 
